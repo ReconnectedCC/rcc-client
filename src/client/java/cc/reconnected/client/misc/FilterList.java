2c6bc5db9a2c3f54903fb8033636a5daaba99cd8
Herr Katze <scmcgowen@gmail.com>
2026-06-02 00:11:07 -0700

    Change server hash to actually be correct

--- src/client/java/cc/reconnected/client/misc/FilterList.java
+++ src/client/java/cc/reconnected/client/misc/FilterList.java
@@ -9,7 +9,7 @@ import java.util.HexFormat;
 import java.util.List;
 
 public class FilterList {
-    private static final List<String> BLACKLIST = List.of("6799efcd3b9f3f86eeffe23de95d439a695620247b2ab951b8c987ecc2f1373ced88f4475192383d8f84a27d9e2104d8451ae5a339af49d683206a32e25d5dc5","8d5fe7f6f1864e720e0f13513ce9d394f492ddf690e68e0cb1e397ff129eec9f");
+    private static final List<String> BLACKLIST = List.of("6799efcd3b9f3f86eeffe23de95d439a695620247b2ab951b8c987ecc2f1373ced88f4475192383d8f84a27d9e2104d8451ae5a339af49d683206a32e25d5dc5","6a86e5f651b0fb60f6e7a2935ebf3fdedb44d2ffe8c5a0e04ca3aad2977c0010724619ff0dd6e61b41e62a98140a8aed65ef9ab2400a10baf4b457ea4ab0b66a");
     private static final List<String> BYPASS = List.of("6ae36085ce66c9bfaac2aa73baa13353c7b03cea8a8fa5e55e6fb33df79c8e431c14b3f7353869a47d8e3051ed02117432563bcd851edf916895515def5ba974");
 
     private static final String ENV_NAME = "FILTER_PW";
